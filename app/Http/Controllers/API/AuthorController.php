791373d23520aa1396b999a64dc28553ffb3264a
LastWord7722 <LastWordMark@gmail.com>
2023-02-25 14:09:27 +0200

    create popup for book

---
+++ app/Http/Controllers/API/AuthorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index(){
+        return BookResource::collection(Book::all());
+    }
+
+    public function delete(Book $book){
+        $book->delete();
+        return response([]);
+    }
+}
---
+++ resources/js/components/library/v-popup/v-popup.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "v-popup"
+}
+</script>
+
+<style scoped>
+
+</style>
